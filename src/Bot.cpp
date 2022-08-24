a673540d6fd43a005fe08a23a343830de51d1f0e
PJutch <103033969+PJutch@users.noreply.github.com>
2022-08-24 21:58:13 +0300

    Added missing licence notices

--- src/Bot.cpp
+++ src/Bot.cpp
@@ -1,3 +1,16 @@
+/* This file is part of JCyberEvolution.
+
+JCyberEvolution is free software: you can redistribute it and/or modify it 
+under the terms of the GNU General Public License as published by the Free Software Foundation, 
+either version 3 of the License, or (at your option) any later version.
+
+JCyberEvolution is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with JCyberEvolution. 
+If not, see <https://www.gnu.org/licenses/>. */
+
 #include "Bot.h"
 #include "utility.h"
 
@@ -8,7 +21,8 @@ using sf::RenderTarget;
 using sf::RenderStates;
 
 Bot::Bot(Vector2f position, Color color) noexcept : 
-        m_shape{{0.7f, 0.7f}}, m_directionShape{{0.1f, 0.3f}}, m_shouldDrawDirection{false} {
+        m_shape{{0.7f, 0.7f}}, m_directionShape{{0.1f, 0.3f}}, m_shouldDrawDirection{false
+        } {
     Color outlineColor = getOutlineColorFor(color);
 
     m_shape.setOrigin(-0.15f, -0.15f);
